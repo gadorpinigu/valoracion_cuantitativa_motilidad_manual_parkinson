0832b590cfd51a35ce8d7a1dadf2b07304d52324
gadorpinigu <93770487+gadorpinigu@users.noreply.github.com>
2022-08-26 20:31:54 +0200

    Update TTK_videoinput.m

--- TTK_videoinput.m
+++ TTK_videoinput.m
@@ -1,3 +1,6 @@
+% % % SCRIPT ORIGINAL: PROYECTO TTKT
+% ADAPTADO PARA TEST PARKINSON: GADOR PIÑEYRO, UNAV
+
 function vid = TTK_videoinput(n)
 
 k = imaqhwinfo('tisimaq_r2013');
@@ -5,7 +8,7 @@ Info = k.DeviceInfo.SupportedFormats;
 vid = videoinput('tisimaq_r2013',n,Info{1,127});
 %Configuramos las propiedades del trigger
 triggerconfig(vid,'manual') 
-%Una vez el v�deo empieza, el trigger se dispara continuamente hasta que se
+%Una vez el vídeo empieza, el trigger se dispara continuamente hasta que se
 %le da la orden de STOP
 set(vid, 'TriggerRepeat', Inf);
 %El valor default de FramesPerTrigger es 10
